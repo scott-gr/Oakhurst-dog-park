0f6f615295b47e59588f3deaa3f1ed28bd5b008f
Scott Griffin <mscottgriffin@gmail.com>
2020-12-31 14:35:02 -0500

    refactor blog template

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,12 +3,29 @@
  */
 
 module.exports = {
+  siteMetadata: {
+    title: `Oakhurst Dog Park`,
+    author: {
+      name: `Scott Griffin`,
+      summary: `Web developer based in Atlanta - https://yoscott.com `,
+    },
+    description: `A community page with guidelines and news about the Oakhurst Dog Park`,
+    siteUrl: `https://oakhurstdogpark.com`,
+  },
+
   plugins: [
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `blog`,
-        path: `${__dirname}/blog`,
+        path: `${__dirname}/content/blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
       },
     },
     `gatsby-transformer-remark`,
--- src/templates/blogTemplate/blogTemplate.js
+++ src/templates/blogTemplate/blogTemplate.js
@@ -1,33 +1,112 @@
 import React from "react"
 import { graphql } from "gatsby"
 import styles from "./blogTemplate.module.css"
+import Layout from '../../components/Layout/Layout.js'
 
-export default function Template({
-  data, // this prop will be injected by the GraphQL query below.
-}) {
-  const { markdownRemark } = data // data.markdownRemark holds your post data
-  const { frontmatter, html } = markdownRemark
+// export default function BlogPostTemplate({
+//   data, // this prop will be injected by the GraphQL query below.
+// }) {
+//   const { markdownRemark } = data // data.markdownRemark holds your post data
+//   const { frontmatter, html } = markdownRemark
+//   return (
+//     <div className={styles.blogContainer}>
+//       <div className={styles.blogPost}>
+//         <h1>{frontmatter.title}</h1>
+//         <h2>{frontmatter.date}</h2>
+//         <div
+//           className={styles.blogContent}
+//           dangerouslySetInnerHTML={{ __html: html }}
+//         />
+//       </div>
+//     </div>
+//   )
+// }
+
+const BlogPostTemplate = ({ data, location }) => {
+  const post = data.markdownRemark
+  const siteTitle = data.site.siteMetadata?.title || `Title`
+  const { previous, next } = data
+
   return (
-    <div className={styles.blogContainer}>
-      <div className={styles.blogPost}>
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
-        <div
-          className={styles.blogContent}
-          dangerouslySetInnerHTML={{ __html: html }}
+    <Layout location={location} title={siteTitle}>
+      <SEO
+        title={post.frontmatter.title}
+        description={post.frontmatter.description || post.excerpt}
+      />
+      <article
+        className={styles.blogPostContent}
+        itemScope
+        itemType="http://schema.org/Article"
+      >
+        <header>
+          <h1 itemProp="headline">{post.frontmatter.title}</h1>
+          <p>{post.frontmatter.date}</p>
+        </header>
+        <section
+          dangerouslySetInnerHTML={{ __html: post.html }}
+          itemProp="articleBody"
         />
-      </div>
-    </div>
+        <hr />
+        <footer>
+
+        </footer>
+      </article>
+      <nav className={styles.blognav}>
+        <ul>
+          <li>
+            {previous && (
+              <Link to={previous.fields.slug} rel="prev">
+                ← {previous.frontmatter.title}
+              </Link>
+            )}
+          </li>
+          <li>
+            {next && (
+              <Link to={next.fields.slug} rel="next">
+                {next.frontmatter.title} →
+              </Link>
+            )}
+          </li>
+        </ul>
+      </nav>
+    </Layout>
   )
 }
 
 export const pageQuery = graphql`
-  query($slug: String!) {
-    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+  query BlogPostBySlug(
+    $id: String!
+    $previousPostId: String
+    $nextPostId: String
+  ) {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    markdownRemark(id: { eq: $id }) {
+      id
+      excerpt(pruneLength: 160)
       html
       frontmatter {
+        title
         date(formatString: "MMMM DD, YYYY")
+        description
+      }
+    }
+    previous: markdownRemark(id: { eq: $previousPostId }) {
+      fields {
         slug
+      }
+      frontmatter {
+        title
+      }
+    }
+    next: markdownRemark(id: { eq: $nextPostId }) {
+      fields {
+        slug
+      }
+      frontmatter {
         title
       }
     }
