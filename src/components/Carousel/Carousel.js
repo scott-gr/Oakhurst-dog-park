1657ada578262d554038e69afcffe9c8651c8a07
Scott Griffin <mscottgriffin@gmail.com>
2021-01-09 14:45:11 -0500

    background carousel transitions work, test again with final images

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -7,15 +7,16 @@ const Carousel = ({ children }) => (
   <>
     <main>{children}</main>
     <BackgroundSlider
-      className="masterhead"
+      className={styles.masthead}
       query={useStaticQuery(graphql`
         query {
-          backgrounds: allFile (filter: {sourceInstanceName: {eq: "images"}}){
+          backgrounds: allFile (filter: {sourceInstanceName: {eq: "logos"}}){
             nodes {
               relativePath
               childImageSharp {
-                fluid (maxWidth: 1000, quality: 100){
-                  ...GatsbyImageSharpFluid
+                fluid (maxWidth: 800, quality: 100,){
+                  ...GatsbyImageSharpFluid_noBase64
+
                 }
               }
             }
@@ -23,20 +24,19 @@ const Carousel = ({ children }) => (
         }
       `)}
       initDelay={2} // delay before the first transition (if left at 0, the first image will be skipped initially)
-      transition={4} // transition duration between images
-      duration={8} // how long an image is shown
+      transition={3} // transition duration between images
+      duration={6} // how long an image is shown
       // specify images to include (and their order) according to `relativePath`
-      images={["background-01.png", "background-02.png", "background-03.png", "background-04.png" ]}
+      images={["transblack.png", "transwhite.png", "tricolor.png", "white.png" ]}
 
       // pass down standard element props
       style={{
-        transform: "scale(.77)" }}
+        transform: "scale(.9)",
+        height: "fit-content",
+        top: "-10%",
+        bottom: "-4rem"
+ }}
       >
-      {/* Captions in sync with background images*/}
-      <div>Portfolio</div>
-      <div>Assets</div>
-      <div>AdvisorS</div>
-      <div>Advisor</div>
             
     </BackgroundSlider>
   </>
