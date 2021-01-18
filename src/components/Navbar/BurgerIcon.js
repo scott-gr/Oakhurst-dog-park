0f141742e16bc40021f251c4f4e54ef55ae65c7a
Scott Griffin <mscottgriffin@gmail.com>
2021-01-18 15:18:34 -0500

    burger button css toggling, needs work

--- src/components/Navbar/BurgerIcon.js
+++ src/components/Navbar/BurgerIcon.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars } from '@fortawesome/free-solid-svg-icons'
+import React from "react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBars } from "@fortawesome/free-solid-svg-icons"
 
 const BurgerIcon = () => {
   return (
+    <>
       <FontAwesomeIcon icon={faBars} />
-  );
-};
+    </>
+  )
+}
 
-export default BurgerIcon;
+export default BurgerIcon
