122fbd95e062228cc734c31b976b9a745e0cc7ba
Scott Griffin <mscottgriffin@gmail.com>
2021-02-14 10:47:03 -0500

    fixed collection syntax

--- src/pages/rules.js
+++ src/pages/rules.js
@@ -1,356 +1,87 @@
 import React from "react"
+import { Link, graphql } from "gatsby"
 import Layout from "../components/Layout/Layout.js"
 import SEO from "../components/seo.js"
-import { graphql } from "gatsby"
-import "./styles/rules.module.css"
+import "../pages/styles/rules.module.css"
 
-const RulesPage = ({ data, location }) => {
+const RuleIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
+  const rules = data.allMarkdownRemark.nodes
+
+  if (rules.length === 0) {
+    return (
+      <Layout location={location} title={siteTitle}>
+        <SEO title="All rules" />
+        <p>
+          No rules found. Add markdown rules to "content/rules" (or the
+          directory you specified for the "gatsby-source-filesystem" plugin in
+          gatsby-config.js).
+        </p>
+      </Layout>
+    )
+  }
 
   return (
     <Layout location={location} title={siteTitle}>
-      <SEO title="Rules" />
-      {/* <h2 className="header">Frequently Asked Questions</h2> */}
-      <div styleName="collapsedAccordian">
-        <svg xmlns="http://www.w3.org/2000/svg">
-          <symbol viewBox="0 0 24 24" id="expandMore">
-            <path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z" />
-            <path d="M0 0h24v24H0z" fill="none" />
-          </symbol>
-          <symbol viewBox="0 0 24 24" id="close">
-            <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
-            <path d="M0 0h24v24H0z" fill="none" />
-          </symbol>
-        </svg>
-      </div>
-      <section styleName="faqs">
-
-        {/* This is one box */}
-      <details open>
-        <summary> {/* Question goes here */}
-          Does this product have what I need?
-          <svg
-            styleName="controlIcon controlIconExpand"
-            width="24"
-            height="24"
-            role="presentation"
-          >
-            <use
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-              xlinkHref="#expandMore"
-            />
-          </svg>
-          <svg
-            styleName="controlIcon controlIconClose"
-            width="24"
-            height="24"
-            role="presentation"
-          >
-            <use
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-              xlinkHref="#close"
-            />
-          </svg>
-        </summary>
-        <p>Totally. Totally does.</p> {/* Answer goes here */}
-      </details>
-
-
-      <details>
-        <summary>
-          Can I use it all the time?
-          <svg
-            styleName="controlIcon controlIconExpand"
-            width="24"
-            height="24"
-            role="presentation"
-          >
-            <use
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-              xlinkHref="#expandMore"
-            />
-          </svg>
-          <svg
-            styleName="controlIcon controlIconClose"
-            width="24"
-            height="24"
-            role="presentation"
-          >
-            <use
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-              xlinkHref="#close"
-            />
-          </svg>
-        </summary>
-        <p>Of course you can, we won't stop you.</p>
-      </details>
-      <details>
-        <summary>
-          Are there any restrictions?
-          <svg
-            styleName="controlIcon controlIconExpand"
-            width="24"
-            height="24"
-            role="presentation"
-          >
-            <use
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-              xlinkHref="#expandMore"
-            />
-          </svg>
-          <svg
-            styleName="controlIcon controlIconClose"
-            width="24"
-            height="24"
-            role="presentation"
-          >
-            <use
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-              xlinkHref="#close"
-            />
-          </svg>
-        </summary>
-        <p>Only your imagination my friend. Go forth!</p>
-      </details>
-      <details>
-        <summary>
-          Are there any restrictions?
-          <svg
-            styleName="controlIcon controlIconExpand"
-            width="24"
-            height="24"
-            role="presentation"
-          >
-            <use
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-              xlinkHref="#expandMore"
-            />
-          </svg>
-          <svg
-            styleName="controlIcon controlIconClose"
-            width="24"
-            height="24"
-            role="presentation"
-          >
-            <use
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-              xlinkHref="#close"
-            />
-          </svg>
-        </summary>
-        <p>Only your imagination my friend. Go forth!</p>
-      </details>
-      <details>
-        <summary>
-          Are there any restrictions?
-          <svg
-            styleName="controlIcon controlIconExpand"
-            width="24"
-            height="24"
-            role="presentation"
-          >
-            <use
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-              xlinkHref="#expandMore"
-            />
-          </svg>
-          <svg
-            styleName="controlIcon controlIconClose"
-            width="24"
-            height="24"
-            role="presentation"
-          >
-            <use
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-              xlinkHref="#close"
-            />
-          </svg>
-        </summary>
-        <p>Only your imagination my friend. Go forth!</p>
-      </details>
-      <details>
-        <summary>
-          Are there any restrictions?
-          <svg
-            styleName="controlIcon controlIconExpand"
-            width="24"
-            height="24"
-            role="presentation"
-          >
-            <use
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-              xlinkHref="#expandMore"
-            />
-          </svg>
-          <svg
-            styleName="controlIcon controlIconClose"
-            width="24"
-            height="24"
-            role="presentation"
-          >
-            <use
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-              xlinkHref="#close"
-            />
-          </svg>
-        </summary>
-        <p>Only your imagination my friend. Go forth!</p>
-      </details>
-      <details>
-        <summary>
-          Are there any restrictions?
-          <svg
-            styleName="controlIcon controlIconExpand"
-            width="24"
-            height="24"
-            role="presentation"
-          >
-            <use
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-              xlinkHref="#expandMore"
-            />
-          </svg>
-          <svg
-            styleName="controlIcon controlIconClose"
-            width="24"
-            height="24"
-            role="presentation"
-          >
-            <use
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-              xlinkHref="#close"
-            />
-          </svg>
-        </summary>
-        <p>Only your imagination my friend. Go forth!</p>
-      </details>
-      <details>
-        <summary>
-          Are there any restrictions?
-          <svg
-            styleName="controlIcon controlIconExpand"
-            width="24"
-            height="24"
-            role="presentation"
-          >
-            <use
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-              xlinkHref="#expandMore"
-            />
-          </svg>
-          <svg
-            styleName="controlIcon controlIconClose"
-            width="24"
-            height="24"
-            role="presentation"
-          >
-            <use
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-              xlinkHref="#close"
-            />
-          </svg>
-        </summary>
-        <p>Only your imagination my friend. Go forth!</p>
-      </details>
-      <details>
-        <summary>
-          Are there any restrictions?
-          <svg
-            styleName="controlIcon controlIconExpand"
-            width="24"
-            height="24"
-            role="presentation"
-          >
-            <use
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-              xlinkHref="#expandMore"
-            />
-          </svg>
-          <svg
-            styleName="controlIcon controlIconClose"
-            width="24"
-            height="24"
-            role="presentation"
-          >
-            <use
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-              xlinkHref="#close"
-            />
-          </svg>
-        </summary>
-        <p>Only your imagination my friend. Go forth!</p>
-      </details>
-      <details>
-        <summary>
-          Are there any restrictions?
-          <svg
-            styleName="controlIcon controlIconExpand"
-            width="24"
-            height="24"
-            role="presentation"
-          >
-            <use
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-              xlinkHref="#expandMore"
-            />
-          </svg>
-          <svg
-            styleName="controlIcon controlIconClose"
-            width="24"
-            height="24"
-            role="presentation"
-          >
-            <use
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-              xlinkHref="#close"
-            />
-          </svg>
-        </summary>
-        <p>Only your imagination my friend. Go forth!</p>
-      </details>
-      <details>
-        <summary>
-          Are there any restrictions?
-          <svg
-            styleName="controlIcon controlIconExpand"
-            width="24"
-            height="24"
-            role="presentation"
-          >
-            <use
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-              xlinkHref="#expandMore"
-            />
-          </svg>
-          <svg
-            styleName="controlIcon controlIconClose"
-            width="24"
-            height="24"
-            role="presentation"
-          >
-            <use
-              xmlnsXlink="http://www.w3.org/1999/xlink"
-              xlinkHref="#close"
-            />
-          </svg>
-        </summary>
-        <p>Only your imagination my friend. Go forth!</p>
-      </details>
-      {" "}
-      
-      </section>
-      
+      <SEO title="All rules" />
+      <ol style={{ listStyle: `none` }}>
+        {rules.map(rule => {
+          const rulebrief = rule.frontmatter.rulebrief|| rule.fields.slug
+          const category = rule.frontmatter.category
+          return (
+            <li key={rule.fields.slug} >
+              <article
+                styleName="ruleCard"
+                // className="rule-list-item"
+                itemScope
+                itemType="http://schema.org/Article"
+              >
+                <header>
+                  <h2 styleName="ruleBrief">
+                    <Link to={rule.fields.slug} itemProp="url">
+                      <span itemProp="headline">{rulebrief}</span>
+                    </Link>
+                  </h2>
+                  {/* <small styleName="ruleDate">{rule.frontmatter.date}</small> */}
+                </header>
+                <section>
+                  <p
+                    styleName="ruleDesc"
+                    dangerouslySetInnerHTML={{
+                      __html: rule.frontmatter.description || rule.excerpt,
+                    }}
+                    itemProp="description"
+                  />
+                </section>
+              </article>
+            </li>
+          )
+        })}
+      </ol>
     </Layout>
   )
 }
 
-export default RulesPage
+export default RuleIndex
 
 export const pageQuery = graphql`
   query {
     site {
       siteMetadata {
-        title
+        rulebrief
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      nodes {
+        excerpt
+        fields {
+          slug
+        }
+        frontmatter {
+          date(formatString: "MMMM DD, YYYY")
+          rulebrief
+          description
+        }
       }
     }
   }
