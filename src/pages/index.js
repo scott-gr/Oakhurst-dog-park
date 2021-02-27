1cc08502f52f6ca4cbf13a5ff9368e1a1dcd3803
Scott Griffin <mscottgriffin@gmail.com>
2021-02-27 13:45:53 -0500

    restored photo slider to b
    ase on breakpoints

--- src/pages/index.js
+++ src/pages/index.js
@@ -14,8 +14,8 @@ const HomePage = ({ data, location }) => {
     <Layout location={location} title={siteTitle}>
       <SEO title="Home Page" />
 
-      <h2 styleName="bigText">Oakhurst Dog Park</h2>
-      {/* <h4 styleName="smallText">Some really great, eye-catching words about the dog park. This text is fun, not necessarily informative.</h4> */}
+      <h2 styleName="bigText"></h2>
+      <h4 styleName="smallText">Some really great, eye-catching words about the dog park. This text is fun, not necessarily informative.</h4>
 
       <div styleName="overlay" />
       <PhotoSlider />
