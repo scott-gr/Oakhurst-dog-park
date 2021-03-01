36672796c1ca0cb8e0f3f6f494dfec6b3f5324bc
Scott Griffin <mscottgriffin@gmail.com>
2021-03-01 11:12:22 -0500

    logo resize, aligning divs on index page

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,33 +1,45 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
+import { useBreakpoint } from "gatsby-plugin-breakpoints"
 import Img from "gatsby-image"
 import { Link } from "gatsby"
 import "./hero.module.css"
 
+// queries for mobile and desktop sized icons
 const Hero = () => {
   const data = useStaticQuery(graphql`
     query {
-      heroImage: file(relativePath: { eq: "tricolor.png" }) {
+      heroMobile: file(relativePath: { eq: "tricolor.png" }) {
         childImageSharp {
-          fixed(height: 100, pngCompressionSpeed: 10) {
+          fixed(height: 80, pngCompressionSpeed: 10) {
+            ...GatsbyImageSharpFixed_withWebp
+          }
+        }
+      }
+      heroDesktop: file(relativePath: { eq: "tricolor.png" }) {
+        childImageSharp {
+          fixed(height: 150, pngCompressionSpeed: 10) {
             ...GatsbyImageSharpFixed_withWebp
           }
         }
       }
     }
   `)
+  const breakpoints = useBreakpoint()
+  const heroImage = breakpoints.mobileXl
+    ? data.heroMobile.childImageSharp.fixed
+    : data.heroDesktop.childImageSharp.fixed
 
-  if (!data?.heroImage?.childImageSharp?.fixed) {
+  if (
+    !data?.heroMobile?.childImageSharp?.fixed ||
+    !data?.heroDesktop?.childImageSharp?.fixed
+  ) {
     return <div>Picture not found</div>
   }
 
   return (
     <Link styleName="heroLink" to="/">
-      <Img
-        styleName="hero"
-        fixed={data.heroImage.childImageSharp.fixed}
-        durationFadeIn={100}
-      />
+      <Img styleName="hero" fixed={heroImage} durationFadeIn={100} />
     </Link>
   )
 }
