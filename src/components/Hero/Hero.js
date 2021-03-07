7533aa0644dd8859f77564d20b22fb15d7fa87bc
Scott Griffin <mscottgriffin@gmail.com>
2021-03-07 04:18:13 -0500

    layout mobile alignment

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,24 +1,63 @@
 import React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
+import { useBreakpoint } from 'gatsby-plugin-breakpoints'
 import Img from 'gatsby-image'
+import { Link } from 'gatsby'
 import './hero.module.css'
 
 // queries for mobile and desktop sized icons
+// const Hero = () => {
+//   const data = useStaticQuery(graphql`
+//     query {
+//       heroImage: file(relativePath: { eq: "tricolorwhiteshadow.png" }) {
+//         childImageSharp {
+//           fixed(height: 240, quality: 100) {
+//             ...GatsbyImageSharpFixed_withWebp
+//           }
+//         }
+//       }
+//     }
+//   `)
+//   const heroImage = data.heroImage.childImageSharp.fixed
+
+//   return <Img styleName="hero" fixed={heroImage} durationFadeIn={50} />
+// }
+
+// export default Hero
 const Hero = () => {
   const data = useStaticQuery(graphql`
     query {
-      heroImage: file(relativePath: { eq: "tricolorwhiteshadow.png" }) {
+      heroMobile: file(relativePath: { eq: "tricolorwhiteshadow.png" }) {
         childImageSharp {
-          fixed(height: 240, quality: 100) {
+          fixed(height: 235, quality: 100) {
+            ...GatsbyImageSharpFixed_withWebp
+          }
+        }
+      }
+      heroDesktop: file(relativePath: { eq: "tricolorwhiteshadow.png" }) {
+        childImageSharp {
+          fixed(height: 300, quality: 100) {
             ...GatsbyImageSharpFixed_withWebp
           }
         }
       }
     }
   `)
-  const heroImage = data.heroImage.childImageSharp.fixed
+  const breakpoints = useBreakpoint()
+  const heroImage = breakpoints.pc
+    ? data.heroMobile.childImageSharp.fixed
+    : data.heroDesktop.childImageSharp.fixed
 
-  return <Img styleName="hero" fixed={heroImage} durationFadeIn={50} />
+  if (
+    !data?.heroMobile?.childImageSharp?.fixed ||
+    !data?.heroDesktop?.childImageSharp?.fixed
+  ) {
+    return <div>Picture not found</div>
+  }
+
+  return (
+      <Img styleName="hero" fixed={heroImage} durationFadeIn={100} />
+  )
 }
 
 export default Hero
