d39ea82b1a3987a1ce1f1aaf729e99f733fa877f
Scott Griffin <mscottgriffin@gmail.com>
2021-03-08 16:09:08 -0500

    successful sticky footer in layout. Layout now doesn't unmount with wrapPageElement API in gatsby-browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,12 @@
 import './src/css/style.css'
 import './src/css/normalize.css'
+import React from 'react'
+import Layout from "./src/components/Layout/Layout.js"
+
+const wrapPageElement = ({ element, props }) => {
+  // props provide same data to Layout as Page element will get
+  // including location, data, etc - you don't need to pass it
+  return <Layout {...props}>{element}</Layout>
+}
+
+export default wrapPageElement
