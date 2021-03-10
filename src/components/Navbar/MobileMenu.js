c73e177c221982d3a840cd4c16aae021c6b11335
Scott Griffin <mscottgriffin@gmail.com>
2021-03-09 23:49:16 -0500

    menu working. Needs css tweaks to be great

--- src/components/Navbar/MobileMenu.js
+++ src/components/Navbar/MobileMenu.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react'
 import Menu from 'react-burger-menu/lib/menus/slide'
 import NavbarLinks from './NavbarLinks.js'
-import "./menu.css"
+import NavIcon from "./NavIcon.js"
+import './menu.css'
 class MobileMenu extends Component {
   showSettings(event) {
     event.preventDefault()
-    
   }
 
-  
   render() {
-    
     return (
-      <Menu  right width="100%">
+      <Menu right width="100%">
+        <NavIcon />
         <NavbarLinks className="menu-item" />
       </Menu>
     )
