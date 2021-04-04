3452343f46893c3fe6db45267c9aa805176c06ca
Scott Griffin <mscottgriffin@gmail.com>
2021-04-04 16:57:36 -0400

    fix config path

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `uploads`,
-        path: `${__dirname}/static/images/uploads`,
+        path: `${__dirname}/static/uploads`,
       },
     },
     {
